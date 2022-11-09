6109c1a97586e96fee63e49676f2b05c5912a031
AngeloHO <angelo.oneda77@hotmail.com>
2022-11-09 15:35:54 -0300

    Exercicio Tupla

---
+++ Tuplas.py
@@ -0,0 +1,34 @@
+tupla = ('Palmeiras', 
+'Internacional', 
+'Fluminense', 
+'Flamengo', 
+'Athletico-PR', 
+'América-MG' , 
+'Atlético-MG', 
+'São Paulo' , 
+'Botafogo' , 
+'Fortaleza' , 
+'Santos', 
+'Goiás', 
+'Bragaentino', 
+'Coritiba', 
+'Cuiabá', 
+'Atlético-GO', 
+'Ceára', 
+'Avaí', 
+'Juventude')
+print(tupla)
+print('Os cinco primeiros colocados são:')
+print(tupla [0] '/t')
+print(tupla [1])
+print(tupla [2])
+print(tupla [3])
+print(tupla [4])
+print('Os quatro últimos colocados são:')
+print(tupla [15])
+print(tupla [16])
+print(tupla [17])
+print(tupla [18])
+print(sorted(tupla))
+
+print('O',tupla [1], '2º colocação.')
